6a3db70333cf4ef50229277f55d38caf5ad171c0
Matt Canty <matthewcanty@gmail.com>
2019-10-08 13:38:38 +0100

    Refactor comments into a kinda markdown style

--- new-mac.sh
+++ new-mac.sh
@@ -1,14 +1,11 @@
 #!/bin/bash
 
-# Install homebrew
+# Prerequisites
+## Homebrew
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
-# Install zsh, Oh My Zsh and switch to it
-brew install zsh
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-
- ####### System Preferences #######
-# Dock
+# System Preferences
+## Dock
 defaults write com.apple.dock orientation -string bottom
 defaults write com.apple.dock "show-recents" -int 0
 defaults write com.apple.dock tilesize -int 37
@@ -16,19 +13,19 @@ defaults write com.apple.dock largesize -int 49
 defaults write com.apple.dock autohide -int 1
 defaults write com.apple.dock magnification -int 1
 defaults write com.apple.dock "minimize-to-application" -int 1
-# Hot Corners
+## Hot Corners
 defaults write com.apple.dock "wvous-br-corner" -int 4 # Show Desktop
 defaults write com.apple.dock "wvous-br-modifier" -int 0
 defaults write com.apple.dock "wvous-tr-corner" -int 10 # Lock Screen
 defaults write com.apple.dock "wvous-tr-modifier" -int 0
-# Finder
+## Finder
 defaults write com.apple.finder AppleShowAllFiles -string YES
 defaults write com.apple.finder ShowStatusBar -int 1
 defaults write com.apple.finder ShowPathbar -int 1
 defaults write com.apple.finder SidebarTagsSctionDisclosedState -int 0
 defaults write com.apple.finder SidebarPlacesSectionDisclosedState -int 1
 defaults write com.apple.finder SidebarDevicesSectionDisclosedState -int 0
-# Other
+## Other
 defaults write ~/Library/Preferences/.GlobalPreferences.plist "com.apple.trackpad.forceClick" -int 0
 defaults write ~/Library/Preferences/.GlobalPreferences.plist "WebAutomaticSpellingCorrectionEnabled" -int 0
 defaults write ~/Library/Preferences/.GlobalPreferences.plist "AppleActionOnDoubleClick" -string "Maximize"
@@ -39,13 +36,13 @@ defaults write ~/Library/Preferences/.GlobalPreferences.plist "NSAutomaticDashSu
 defaults write ~/Library/Preferences/.GlobalPreferences.plist "NSAutomaticPeriodSubstitutionEnabled" -int 0
 defaults write ~/Library/Preferences/.GlobalPreferences.plist "NSAutomaticQuoteSubstitutionEnabled" -int 0
 defaults write ~/Library/Preferences/.GlobalPreferences.plist "NSAutomaticSpellingCorrectionEnabled" -int 0
-# Battery
+## Battery
 defaults write com.apple.menuextra.battery ShowPercent YES
-# Clock
+## Clock
 defaults write com.apple.menuextra.clock DateFormat -string "EEE d MMM  HH:mm:ss"
 defaults write com.apple.menuextra.clock FlashDateSeparators int 0
 defaults write com.apple.menuextra.clock IsAnalog -int 0
-# Three finger drag
+## Three finger drag
 defaults write ~/Library/Preferences/com.apple.driver.AppleBluetoothMultitouch.trackpad.plist "TrackpadThreeFingerDrag" -int 1 
 defaults write ~/Library/Preferences/com.apple.AppleMultitouchTrackpad.plist "TrackpadThreeFingerDrag" -int 1
 defaults write ~/Library/Preferences/com.apple.driver.AppleBluetoothMultitouch.trackpad.plist "TrackpadThreeFingerHorizSwipeGesture" -int 0
@@ -53,23 +50,22 @@ defaults write ~/Library/Preferences/com.apple.AppleMultitouchTrackpad.plist "Tr
 defaults write ~/Library/Preferences/com.apple.driver.AppleBluetoothMultitouch.trackpad.plist "Clicking" -int 1 
 defaults write ~/Library/Preferences/com.apple.AppleMultitouchTrackpad.plist "Clicking" -int 1
 
-# Desktop
-killall Dock
-killall Finder
-killall SystemUIServer
-
-####### Additional Software #######
-# Slack
+# Additional Software and Tools
+## Slack
 brew cask install slack
-# Spotify
+## Spotify
 brew cask install spotify
-# IntelliJ IDEA
+## IntelliJ IDEA
 brew cask install intellij-idea
-# CircleCI CLI
+## CircleCI CLI
 curl -fLSs https://circle.ci/cli | bash
-
-####### Dock Cleanup #######
+## Oh My Zsh
+brew install zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+## Dockutil
 brew install dockutil
+
+# Dock Cleanup
 dockutil --remove 'Mail' --no-restart
 dockutil --remove 'Contacts' --no-restart
 dockutil --remove 'Maps' --no-restart
@@ -91,8 +87,13 @@ dockutil --add "/Applications/IntelliJ IDEA.app" --after 'Safari' --no-restart
 dockutil --add "/Applications/Spotify.app" --after 'Safari' --no-restart
 dockutil --add "/Applications/Slack.app" --after 'Safari' --no-restart
 dockutil --add "/Applications/Utilities/Terminal.app" --after 'Safari' --no-restart
-killall Dock
 
+# Restart Desktop
+killall Dock
+killall Finder
+killall SystemUIServer
+
+# Finito
 echo ""
 echo "All done, cheers 🙌"
 echo "You will need to logout and back in again for some changes to take effect!"
